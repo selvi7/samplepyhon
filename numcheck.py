e5bd52b6c246820e43a579b1e9689f8dd6af456e
selvi <susilasusila1999@gmail.com>
2019-06-30 21:24:26 +0530

    'sdsd'

--- numcheck.py
+++ numcheck.py
@@ -1,8 +1,11 @@
 print('enter a number:')
-x = int(input())
-if x > 0:
-    print('given number is positive')
-elif x < 0:
-    print('given number is negative')
-else:
-    print('number is zero')
+try:
+    x = int(input())
+    if x > 0:
+        print('given number is positive')
+    elif x < 0:
+        print('given number is negative')
+    else:
+        print('number is zero')
+except:
+    print('Invalid input')
