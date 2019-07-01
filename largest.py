8c62f856c58e3e8360fce3695e60ae018604d5f5
selvi <susilasusila1999@gmail.com>
2019-07-01 20:20:49 +0530

    'fdfd'

---
+++ largest.py
@@ -0,0 +1,12 @@
+print ("enter the number")
+try:
+    x=int(input())
+if 10>5:
+    print ("enter the largest number")
+else:
+    print ("not a larger number")
+ except:
+        print("invalid input")
+
+
+    
