6adf5f611a77d8ba32faf6b9708f7f33155149fa
selvi <susilasusila1999@gmail.com>
2019-07-08 12:01:04 +0530

    'fdfd'

---
+++ 21.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the 1st num of A.P:"))
+n=int(input("Enter the total  num of A.P:"))
+d=int(input("Enter the common differece:"))
+total=(n*(2*a+(n-1)*d))/2
+tn=a+(n-1)*d
+print("The sum of A,P=",total)
+print("The tn term of A,P=",tn)
