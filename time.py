7a1bbb14bb53c49b8e60096dbf60efcbe1c113c3
selvi <susilasusila1999@gmail.com>
2019-07-10 12:45:09 +0530

    'fdfd'

--- time.py
+++ time.py
@@ -1,7 +1,7 @@
 time = float(input("Input time in seconds: "))
 day = time // (24 * 3600)
-time = time % (24 * 3600)
 hour = time // 3600
+time = time % (24 * 3600)
 time %= 3600
 minutes = time // 60
 time %= 60
