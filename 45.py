e71b529e80549c50e9d19b93ca13d73f34cfd0b3
selvi <susilasusila1999@gmail.com>
2019-07-11 20:54:19 +0530

    'divi'

--- 45.py
+++ 45.py
@@ -1,6 +1,6 @@
-x=int(input("Enter number:"))
+n=int(input("Enter number:"))
 count=0
-while(x>0):
+while(n>0):
     count=count+1
-    x=x//10
-print("The number of digits in the number are:",count)
+    n=n//10
+print("The number of digits are:",count)
