f087a0dd2af00eab9235b0024e02dbb9f543776f
selvi <susilasusila1999@gmail.com>
2019-07-11 11:20:11 +0530

    'fafa'

---
+++ 37.py
@@ -0,0 +1,13 @@
+a = input('Enter value of a: ')
+b = input('Enter value of b: ')
+
+a = 5
+b = 10
+
+
+temp = a
+a = b
+b = temp
+
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
