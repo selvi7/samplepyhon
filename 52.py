99742f56d1f73b393bcb25145effa32a8f610192
selvi <susilasusila1999@gmail.com>
2019-07-12 20:41:27 +0530

    'dd'

---
+++ 52.py
@@ -0,0 +1,21 @@
+num=int(input("Enter a num"))
+if num ==1:
+    print("one")
+if num ==2:
+    print("Two")
+if num ==3:
+    print("Three")
+if num ==4:
+    print("four")
+if num ==5:
+    print("five")
+if num ==6:
+    print("six")
+if num ==7:
+    print("seven")
+if num ==8:
+    print("eight")
+if num ==9:
+    print("nine")
+if num ==10:
+    print("ten")
