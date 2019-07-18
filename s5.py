07d70bc6cd8fb0003eebadb7a1d9d951ccb06276
selvi <susilasusila1999@gmail.com>
2019-07-18 17:20:54 +0530

    'fadaf'

---
+++ s5.py
@@ -0,0 +1,7 @@
+a1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+b1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+c1=str(input("Enter the num:"))
+for d1 in range(20):
+  if(c1==a1[d1]):
+    print(b1[d1])
+    break
