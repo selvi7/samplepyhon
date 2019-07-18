703793f61e43d87a6bf40f96ec65b9458e9208cd
selvi <susilasusila1999@gmail.com>
2019-07-18 19:50:20 +0530

    'fdfd'

---
+++ s10.py
@@ -0,0 +1,10 @@
+x1 = (input("Enter the num:"))
+y1 = (input("Enter the num:"))
+for i in x1:
+    c1=x1.count(i)
+for j in y1:
+    d1=y1.count(j)
+if c1==d1:
+    print("yes")
+else:
+    print("no")
