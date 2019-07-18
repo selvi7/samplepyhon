56f90a6a8f740e41115dd0887db717a1e12e7960
selvi <susilasusila1999@gmail.com>
2019-07-18 17:33:46 +0530

    'fdfd'

---
+++ s6.py
@@ -0,0 +1,10 @@
+a1 = (input("Enter the num:"))
+b1 = (input("Enter the num:"))
+for i in a1:
+    c1=a1.count(i)
+for j in b1:
+    d1=b1.count(j)
+if c1==d1:
+    print("yes")
+else:
+    print("no")
