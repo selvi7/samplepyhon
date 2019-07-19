6df553706bf2444784f49394a14c0f83e5380f2a
selvi <susilasusila1999@gmail.com>
2019-07-19 13:02:21 +0530

    'fdfd'

--- 96.py
+++ 96.py
@@ -1,11 +1,9 @@
-def volumeCubo( l , h , w ): 
-    return (l * h * w) 
-      
-def surfaceAreaCubo( l , h , w ): 
-    return (2 * l * w + 2 * w * h + 2 * l * h) 
-  
-l = 1
-h =2
-w = 3
-print("Volume =" , volumeCubo(l, h, w)) 
-print("Total Surface of the Area =", surfaceAreaCubo(l, h, w))
+a = int(input("Please Enter the First Num of an A.P Series: : "))
+n = int(input("Please Enter the Total Num in this A.P Series: : "))
+d = int(input("Please Enter the Common Difference : "))
+
+sum = (n * (2 * a + (n - 1) * d)) / 2
+tn = a + (n - 1) * d
+
+print("\nThe Sum of the Arithmetic Progression Series = " , sum)
+print("The tn Term of the Arithmetic Progression Series = " , tn)
