67a44229ad1e1720c48e7898e0879fdf4e3ce5e7
selvi <susilasusila1999@gmail.com>
2019-07-21 20:49:50 +0530

    'fdfd'

---
+++ s31.py
@@ -0,0 +1,12 @@
+mum=input("Enter the num:")
+count1=0
+count2=0
+for i in mum:
+    if i=='(':
+        count1=count1+1
+    else:
+        count2=count2+1
+if(count1==count2):
+    print("yes")
+else:
+    print("no")
