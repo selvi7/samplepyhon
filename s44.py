acb28ee6c01618a60305ddf6ff477f58592a0e17
selvi <susilasusila1999@gmail.com>
2019-07-24 10:52:22 +0530

    'fdfd'

---
+++ s44.py
@@ -0,0 +1,12 @@
+def rotate(input,d): 
+  
+    # slice string in two parts for left and right 
+    Lfirst = input[0 : d] 
+    Lsecond = input[d :] 
+    Rfirst = input[0 : len(input)-d] 
+    Rsecond = input[len(input)-d : ] 
+  
+    # now concatenate two parts together 
+    print ("Left Rotation", "Lsecond + Lfirst") 
+    print ("Right Rotation","Rsecond + Rfirst") 
+
