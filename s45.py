7d7caf843fbd86ba17a78c636dc8c3b270ec0df4
selvi <susilasusila1999@gmail.com>
2019-07-25 11:12:22 +0530

    'fdfd'

--- s45.py
+++ s45.py
@@ -1,3 +1,7 @@
-# returning the value of sine of pi/6 
-math ("The value of sine of pi/6 is : ", end="") 
-print (math.sin(a)) 
+length = float(input('Please Enter the Length of a Triangle: '))
+width = float(input('Please Enter the Width of a Triangle: '))
+
+
+perimeter = 2 * (length + width)
+
+print("Perimeter of a Rectangle using", length, "and", width, " = ", perimeter)
