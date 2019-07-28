c4a1e47d42331a6fa16b322f375ba3bb7d523548
selvi <susilasusila1999@gmail.com>
2019-07-28 20:42:10 +0530

    'fdfd'

---
+++ s79.py
@@ -0,0 +1,14 @@
+x = int(input("enter the num:"))
+y = int(input("enter the num:"))
+
+numberList()
+
+largest = numberList
+
+for num2 in numberList:
+
+    if num2 > largest:
+
+        largest=num2
+
+print(largest)
