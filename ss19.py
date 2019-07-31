27e5d9afc205ef8559f9148a68b765817dcf702f
selvi <susilasusila1999@gmail.com>
2019-07-31 20:10:19 +0530

    'fdfd'

---
+++ ss19.py
@@ -0,0 +1,31 @@
+# function to display first K  
+# maximum sum combinations 
+def KMaxCombinations( A, B, N, K): 
+      
+    # max heap. 
+    pq = PriorityQueue()  
+      
+    # insert all the possible   
+    # combinations in max heap. 
+    for i in range(0, N): 
+        for j in range(0, N): 
+           a = A[i] + B[j]  
+           pq.put((-a, a)) 
+              
+    # pop first N elements from  
+    # max heap and display them. 
+    count = 0
+    while (count < K): 
+        print(pq.get()[1]) 
+        count = count + 1
+          
+# Driver method 
+A = [ 4, 2, 5, 1 ] 
+B = [ 8, 0, 5, 3 ] 
+N = len(A) 
+K = 3
+KMaxCombinations(A, B, N, K) 
+  
+# This code is contributed  
+# by Gitanjali. 
+
