0db87f0071a002cb85e5b94305f15dd228e2df34
selvi <susilasusila1999@gmail.com>
2019-08-01 09:40:44 +0530

    'fdfd'

---
+++ ss39.py
@@ -0,0 +1,7 @@
+s,r=list(input("enter the number:"))
+t,o=list(input("enter the number:"))
+p,q=list(input("enter the number:"))
+if s==r or t==o or p==q or s==t==p or r==o==q:
+    print('yes')
+else:
+    print('no')
