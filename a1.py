c415179836644ab44b7a9e61e31a1446b13881d0
selvi <susilasusila1999@gmail.com>
2019-08-05 13:18:05 +0530

    'fdfd'

---
+++ a1.py
@@ -0,0 +1,9 @@
+x = (input("enter the num:"))
+            
+n = len(x) 
+  
+ans = (x, n) 
+if (len(ans)): 
+    print ("The longest common prefix is")
+else: 
+    print("There is no common prefix") 
