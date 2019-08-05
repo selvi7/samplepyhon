81ba74e9d967096ba35ca9b1696931463dca5787
selvi <susilasusila1999@gmail.com>
2019-08-05 19:57:30 +0530

    'fdfd'

---
+++ a4.py
@@ -0,0 +1,35 @@
+# A Naive recursive Python program to fin minimum number 
+# operations to convert str1 to str2 
+def editDistance(str1, str2, a , b): 
+  
+    # If first string is empty, the only option is to 
+    # insert all characters of second string into first 
+    if m==0: 
+         return b 
+  
+    # If second string is empty, the only option is to 
+    # remove all characters of first string 
+    if b==0: 
+        return a
+  
+    # If last characters of two strings are same, nothing 
+    # much to do. Ignore last characters and get count for 
+    # remaining strings. 
+    if str1[a-1]==str2[b-1]: 
+        return editDistance(str1,str2,a-1,b-1) 
+  
+    # If last characters are not same, consider all three 
+    # operations on last character of first string, recursively 
+    # compute minimum cost for all three operations and take 
+    # minimum of three values. 
+    return 1 + min(editDistance(str1, str2, a, b-1),    # Insert 
+                   editDistance(str1, str2, a-1, b),    # Remove 
+                   editDistance(str1, str2, a-1, b-1)    # Replace 
+                   ) 
+  
+# Driver program to test the above function 
+str1 = "sunday"
+str2 = "saturday"
+print (str1, str2, len(str1), len(str2)) 
+  
+# This code is contributed by Bhavya Jain 
